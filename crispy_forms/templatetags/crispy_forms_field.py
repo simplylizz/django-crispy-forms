3c8403541cb9cbbb2568a03ef9a4929c0437d8e2
Anton Yanchenko <a.ianchenko@catawiki.nl>
2020-10-01 01:19:49 +0200

    Test

--- crispy_forms/templatetags/crispy_forms_field.py
+++ crispy_forms/templatetags/crispy_forms_field.py
@@ -129,7 +129,7 @@ class CrispyFieldNode(template.Node):
                 if field.errors:
                     css_class += " form-control-danger"
 
-            if template_pack == "bootstrap4" and not is_multivalue(field):
+            if template_pack == "bootstrap4":  # and not is_multivalue(field):
                 if not is_checkbox(field):
                     css_class += " form-control"
                     if is_file(field):
